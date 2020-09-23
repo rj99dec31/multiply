52a01a85a96f5168b374d79e6dbb3197ba55a989
rj99dec31 <69425114+rj99dec31@users.noreply.github.com>
2020-09-23 17:46:55 +0530

    Add files via upload

---
+++ 2second.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+int main()
+{
+	char x = 'a';
+	int y = 6;
+	float z = 2.5;
+	double w = 2.34;
+	cout <<"size of int is "<<sizeof(int)<<"bytes"<<endl;
+	cout <<"size of char is "<<sizeof(char)<<"bytes"<<endl;
+	cout <<"size of double is "<<sizeof(double)<<"bytes"<<endl;
+	cout <<"size of float is "<<sizeof(float)<<"bytes"<<endl;
+}
